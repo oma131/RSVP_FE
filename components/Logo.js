87b392e83fb22880914f3d6c0519bb25c16a0ece
oma131 <anosikeihuoma@gmail.com>
2022-09-07 07:24:50 +0100

    new

--- components/Logo.js
+++ components/Logo.js
@@ -4,7 +4,7 @@ const Logo = () => {
     return (
         <Link href="/">
             <a className="my-2 flex items-center space-x-1">
-                <span className="font-bold text-3xl sm:text-lg font-sans tracking-tight whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-lime-600 via-yellow-300 to-red-600 hover:animate-bounce">
+                <span className="font-bold text-lg sm:text-3xl font-sans tracking-tight whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-lime-600 via-yellow-300 to-red-600 hover:animate-bounce">
                     WEB3RSVP
                 </span>
             </a>
